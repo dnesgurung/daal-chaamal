26b6201d0a4f0d59d38c1f6809be7b826dfb468b
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-07-16 13:29:28 +0100

    register and login api complete

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import 'dotenv/config'
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.port || 4000;
@@ -18,6 +19,9 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is working"));
+app.use('/api/user', userRouter)
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

e870717a0f9b9729bce5f121d2038b518af01854
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-07-19 00:57:10 +0100

    product schema added

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import 'dotenv/config'
+import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
 
 const app = express();
 const port = process.env.port || 4000;
 
-await connectDB()
+await connectDB();
 
 //Allow Multiple Origins
 const allowedOrigins = ["http://localhost:5173"];
@@ -19,9 +20,8 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is working"));
-app.use('/api/user', userRouter)
-
-
+app.use("/api/user", userRouter);
+app.use("/api/seller", sellerRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

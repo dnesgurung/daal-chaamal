cbf714a92475eb75b8179a83cf73eee9a91d7afc
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-07-21 14:14:34 +0100

    seller login auth

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { assets, categories } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const AddProduct = () => {
   const [files, setFiles] = useState([]);
@@ -9,25 +12,40 @@ const AddProduct = () => {
   const [price, setPrice] = useState("");
   const [offerPrice, setOfferPrice] = useState("");
 
+  const { axios } = useAppContext();
+
   const onSubmitHandler = async (event) => {
     try {
       event.preventDefault();
-      const {productData} = {
+      const  productData  = {
         name,
-        description: description.split('\n'),
+        description: description.split("\n"),
         category,
         price,
-        offerPrice
-      }
-      
+        offerPrice,
+      };
+
       const formData = new FormData();
-      formData.append('productData', JSON.stringify(productData));
-      for ( let i = 0; i < files.length; i ++){
-        
+      formData.append("productData", JSON.stringify(productData));
+      for (let i = 0; i < files.length; i++) {
+        formData.append("images", files[i]);
       }
 
+      const { data } = await axios.post("/api/product/add", formData);
+
+      if (data.success) {
+        toast.success(data.message);
+        setName("");
+        setDescription("");
+        setCategory("");
+        setPrice("");
+        setOfferPrice("");
+        setFiles([]);
+      } else {
+        toast.error(data.message);
+      }
     } catch (error) {
-      
+      toast.error(error.message);
     }
   };
 
